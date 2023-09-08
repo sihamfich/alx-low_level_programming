287e6d71d4272f52f521d17a3560f6c0f39a9482
sihamfich <sihamfichouche@gmail.com>
2023-09-08 15:05:21 -0700

    Done

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,9 +2,12 @@
 
 /**
  *  main - Entry point
+ *
  *  Description: print alphabet in lowercase then in uppercase
+ *
  *  Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	char ch = 'a';
@@ -16,11 +19,13 @@ int main(void)
 		putchar(ch);
 		ch++;
 	}
+	/*print A-Z*/
 	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
 	}
 	putchar('\n');
+
 	return (0);
 }
