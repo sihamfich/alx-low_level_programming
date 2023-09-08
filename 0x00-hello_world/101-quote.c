b7ef5a4cb2f6de6f7322c992214cda77bdcf790e
sihamfich <sihamfichouche@gmail.com>
2023-09-07 18:07:25 -0700

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print a qoute using write function
+ * size_t write(int fd.const void *buf.size_t count)
+ *
+ * Return: Always 1 (Not Success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
