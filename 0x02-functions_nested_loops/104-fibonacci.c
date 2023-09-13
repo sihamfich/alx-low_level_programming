1e4c40b73ae81c69ac3cbd51d6e01f196d3ebef1
sihamfich <sihamfichouche@gmail.com>
2023-09-13 08:24:48 -0700

    Done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -43,10 +43,11 @@ int main(void)
 		printf("%d", 0);
 		initial0s--;
 		}
+
 		printf("%lu", f1);
 
 		sum = (f1 + f2) % mx;
-		sumo = f1o f2o + (f1 + f2) / mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
 		f1 = f2;
 		f1o = f2o;
 		f2 = sum;
