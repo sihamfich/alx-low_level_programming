56f1385371b986ba6a17e48c08513bb320a732cf
sihamfich <sihamfichouche@gmail.com>
2023-09-14 16:04:18 -0700

    Well Done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -11,15 +12,14 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((!(year % 100 == 0) && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
 			day++;
 		}
-
-		 printf("Day of the year: %d\n", day);
-		 printf("Remaining days: %d\n", 366 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
