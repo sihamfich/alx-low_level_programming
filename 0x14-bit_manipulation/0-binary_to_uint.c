6c03fd56391f5e7808199b2f022716012df58123
SIHAM <sihamfichouche@gmail.com>
2023-10-27 13:03:17 -0700

    Done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,6 +18,9 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);/* Invalid character encountered; return 0.*/
+		Num <<= 1;/*Left-shift the bits*/
+		if (b[i] == '1')
+			Num += 1;
 	}
 	return (Num);
 }
