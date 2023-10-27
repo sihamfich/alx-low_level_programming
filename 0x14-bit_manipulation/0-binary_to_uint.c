a61d837a4c20ebb40137c2c24c8918eb00ce86d7
SIHAM <sihamfichouche@gmail.com>
2023-10-27 12:56:55 -0700

    Done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	int i; /*Index*/
+	unsigned int Num;
+
+	Num = 0;
 	if (!b)
 		return (0);/*Handle the case where the input pointer is NULL.*/
-
-	unsigned int Num = 0;/*Initialize num to 0.*/
-	int i;/*Index*/
-
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);/* Invalid character encountered; return 0.*/
-		Num = (Num << 1) | (b[i] - '0');/* Update num based on the current bit.*/
 	}
-
-	return (Num);/* Return the converted unsigned integer.*/
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		Num <<= 1;/*Left-shift the bits*/
+		if (b[i] == '1')
+			Num += 1;
+	}
+	return (Num);
 }
