776f474d1942a71b5822f2422bea37b05b50f9d0
SIHAM <sihamfichouche@gmail.com>
2023-11-03 16:37:52 -0700

    Done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,22 +5,23 @@
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
- * Return: If a filename == NULL --> -1.
- * text_content == NULL --> create empty file
+ * Return: If a filename == NULL return -1.
+ * text_content == NULL return create empty file
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file, writeToFile, lenght = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (lenght = 0; text_content[lenght];)
-			lenght++;
-	}
+	if (!text_content)
+		text_content = " ";
+	for (lenght = 0; text_content[lenght]; lenght++)
+		;
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	writeToFile = write(file, text_content, lenght);
+	if (writeToFile == -1)
+		return (-1);
+close(file);
 }
