cdd4565e12633265ce036a2323afbc3cba5edc7f
SIHAM <sihamfichouche@gmail.com>
2023-11-10 10:19:32 -0800

    Done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints a number in binary.
+ * @n: number binary to be printed
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int val, i = 0;
+	unsigned long int new;
+
+	for (val = sizeof(n) * 8; val >= 0; val--)
+	{
+		new = n >> val;
+		if (new)
+			_putchar('0');
+		else if (new & 1)
+		{
+			_putchar('1');
+			i++;
+		}
+	}
+	if (!new)
+		_putchar('0');
+}
