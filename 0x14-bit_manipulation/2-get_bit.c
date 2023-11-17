156803a49eca9b80f722b0677db190cbb8e9d347
SIHAM <sihamfichouche@gmail.com>
2023-11-17 03:51:07 -0800

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - function that returns
+ * the value of a bit at a given index.
+ * @n: number to check bits in
+ * @index: index at which to check bit
+ * Return: value of the bit, or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int div, idx;
+
+	if (index > (sizeof(n) * 8 - 1))
+		return (-1);
+	div = 1 << index;
+	idx = n & div;
+	if (div == idx)
+		return (1);
+	return (0);
+}
