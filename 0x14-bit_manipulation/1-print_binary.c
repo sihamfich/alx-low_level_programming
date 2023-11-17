9031efca0f99db4c23dc24931539eb4ef031e8af
SIHAM <sihamfichouche@gmail.com>
2023-11-17 03:15:47 -0800

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,18 @@
 void print_binary(unsigned long int n)
 {
 	int size = sizeof(n) * 8;
-	int binary;
+	int binary = 0;
 
-	for (int i = size - 1; i >= 0; i--)
+	while (size)
 	{
-		binary = (n >> i) & 1;
-		putchar(binary + '0');
+		if (n & 1L << --size)
+		{
+			_putchar('1');
+			binary++;
+		}
+		else if (binary)
+			_putchar('0');
 	}
+	else if (!binary)
+		_putchar('0');
 }
