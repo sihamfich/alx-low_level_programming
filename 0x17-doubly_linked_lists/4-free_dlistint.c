96b8b87201b8f6712461746bbebb14499599ef35
SIHAM <sihamfichouche@gmail.com>
2023-12-07 03:25:52 -0800

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/*
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
